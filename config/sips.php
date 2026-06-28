3ff2d5ab908439f6a88693e9396902652bcd4499
youbrey <138248101+youbrey@users.noreply.github.com>
2026-06-29 00:21:58 +0800

    Add files via upload

--- config/sips.php
+++ config/sips.php
@@ -1,1 +1,33 @@
+<?php
 
+// config/sips.php
+// Pengaturan khusus aplikasi SIPS, terpisah dari config Laravel inti
+// agar mudah dimodifikasi tanpa menyentuh file framework.
+
+return [
+    'instansi' => env('SIPS_INSTANSI', 'DPRD Kota Bitung'),
+    'alamat' => env('SIPS_ALAMAT', ''),
+    'kop_logo' => env('SIPS_KOP_LOGO', 'logo_instansi.png'),
+
+    'jenis_surat' => [
+        'PERJALANAN_DINAS_DPRD' => 'Perjalanan Dinas - DPRD',
+        'PERJALANAN_DINAS_ASN' => 'Perjalanan Dinas - ASN',
+        'PERJALANAN_DINAS_GABUNGAN' => 'Perjalanan Dinas - Gabungan',
+        'UNDANGAN_PARIPURNA' => 'Undangan Rapat Paripurna',
+        'UNDANGAN_BIASA' => 'Undangan Rapat AKD',
+    ],
+
+    'kategori_dprd' => [
+        'PIMPINAN_DPRD' => 'Pimpinan DPRD',
+        'KOMISI_I' => 'Komisi I',
+        'KOMISI_II' => 'Komisi II',
+        'KOMISI_III' => 'Komisi III',
+        'CUSTOM' => 'Lainnya',
+    ],
+
+    'prefix_nomor' => [
+        'surat_tugas' => 'ST',
+        'sppd' => 'SPPD',
+        'undangan' => 'UND',
+    ],
+];
